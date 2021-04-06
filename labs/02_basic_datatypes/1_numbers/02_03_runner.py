9d3f1fc3fd30b506b172a62a7cf80ddb481a739a
milenacudak96 <milenacudak96@gmail.com>
2021-04-06 18:46:30 +0200

    Finished mini_proj

--- labs/02_basic_datatypes/1_numbers/02_03_runner.py
+++ labs/02_basic_datatypes/1_numbers/02_03_runner.py
@@ -4,7 +4,10 @@ If a runner runs 10 miles in 30 minutes and 30 seconds,
 What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
 
 '''
-km = 10 * 1.6
+def converter(int):
+    km = int * 1.6
+    return km
+
 time = 0.5083
-result = km/time
+result = converter(10)/time
 print('th result is: '+ str(result))
